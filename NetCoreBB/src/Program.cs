76b06ee4bf7fcb238711264b027f2b09c5f37e6e
Roman Volkov <info@romanvolkov.io>
2019-07-19 05:09:51 +0200

    Add header comment

---
+++ NetCoreBB/src/Program.cs
@@ -0,0 +1,22 @@
+/**
+ * This file is part of the NetCoreBB forum software package.
+ * @license GNU General Public License, version 3 (GNU GPLv3)
+ * @copyright © 2019 Roman Volkov
+ */
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace NetCoreBB
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+                .Build()
+                .Run();
+        }
+    }
+}
