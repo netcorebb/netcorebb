f9cf1e216799f7a4a168fa092e55199e5f61324f
Roman Volkov <info@romanvolkov.io>
2019-07-21 07:24:55 +0200

    Fix bugs; add unit tests for Verify()

--- NetCoreBB.Tests/src/UnitTests/Infrastructure/PasswordHasherTests.cs
+++ NetCoreBB.Tests/src/UnitTests/Infrastructure/PasswordHasherTests.cs
@@ -4,17 +4,26 @@
  * @copyright © 2019 Roman Volkov
  */
 
+using System;
 using System.Text.RegularExpressions;
 using NetCoreBB.Infrastructure;
 using NetCoreBB.Interfaces;
 using Shouldly;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace NetCoreBB.UnitTests.Infrastructure
 {
     public class PasswordHasherTests
     {
-        public IPasswordHasher Hasher { get; } = new PasswordHasher();
+        private IPasswordHasher Hasher { get; }
+        private ITestOutputHelper Output { get; }
+
+        public PasswordHasherTests(ITestOutputHelper helper)
+        {
+            Hasher = new PasswordHasher();
+            Output = helper;
+        }
 
 
         [Fact]
@@ -56,6 +65,76 @@ namespace NetCoreBB.UnitTests.Infrastructure
             var res4 = Hasher.Hash(null);
 
             new[] {res1, res2, res3, res4}.ShouldAllBe(x => x.IsNone);
+        }
+
+
+        [Fact]
+        public void Verify_works_with_positive()
+        {
+            const string password = "password";
+
+            var res1 = Hasher.Hash(password);
+            res1.IsSome.ShouldBeTrue();
+
+            res1.IfSome(hash => {
+                var res2 = Hasher.Verify(password, hash);
+                res2.IsSome.ShouldBeTrue();
+
+                res2.IfSome(pair => {
+                    var (verify, time) = pair;
+                    verify.ShouldBeTrue();
+                    time.ShouldBeGreaterThan(TimeSpan.FromMilliseconds(10));
+                    Output.WriteLine($"Time elapsed: {time.Milliseconds} ms");
+                });
+            });
+        }
+
+
+        [Fact]
+        public void Verify_works_with_negative()
+        {
+            const string password1 = "password1";
+            const string password2 = "password2";
+
+            var res1 = Hasher.Hash(password1);
+            res1.IsSome.ShouldBeTrue();
+
+            res1.IfSome(hash => {
+                var res2 = Hasher.Verify(password2, hash);
+                res2.IsSome.ShouldBeTrue();
+
+                res2.IfSome(pair => {
+                    var (verify, time) = pair;
+                    verify.ShouldBeFalse();
+                    time.ShouldBeGreaterThan(TimeSpan.FromMilliseconds(10));
+                    Output.WriteLine($"Time elapsed: {time.Milliseconds} ms");
+                });
+            });
+        }
+
+
+        [Fact]
+        public void Verify_refuses_empty_values()
+        {
+            var res1 = Hasher.Verify(null, null);
+            var res2 = Hasher.Verify(null, "abc");
+            var res3 = Hasher.Verify("abc", null);
+
+            var res4 = Hasher.Verify(string.Empty, string.Empty);
+            var res5 = Hasher.Verify(string.Empty, "abc");
+            var res6 = Hasher.Verify("abc", string.Empty);
+
+            new[] {res1, res2, res3, res4, res5, res6}.ShouldAllBe(x => x.IsNone);
+
+            Hasher.Hash("abc").Match(hash => {
+                    var res7 = Hasher.Verify(string.Empty, hash);
+                    var res8 = Hasher.Verify(null, hash);
+                    var res9 = Hasher.Verify("   ", hash);
+                    res7.IsNone.ShouldBeTrue();
+                    res8.IsNone.ShouldBeTrue();
+                    res9.IsNone.ShouldBeTrue();
+                },
+                () => true.ShouldBeFalse("Hashing didn't work."));
         }
     }
 }
