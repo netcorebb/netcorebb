80649259ce3b52e38fd1fbadcf4a2ec245f6dd22
Roman Volkov <info@romanvolkov.io>
2020-06-07 01:31:37 +0200

    Work on Gulpfile

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,13 +1,27 @@
 import {Gulpclass, Task} from "gulpclass/Decorators";
 
 import * as gulp from "gulp";
-import * as del from "del";
+import * as stylus from "gulp-stylus";
+import * as rename from "gulp-rename";
+
+const cleanCss = require("gulp-clean-css");
+
 
 @Gulpclass()
 export class Gulpfile {
 
-    @Task()
-    clean(cb: Function) {
-        return del(["test"]);
+    @Task("style")
+    style() {
+        return gulp.src([
+           // "node_modules/normalize.css/normalize.css",
+            "NetCoreBB.Admin/Styles/global.styl"
+        ])
+            .pipe(stylus({compress: false}))
+            .pipe(cleanCss({debug: true}))
+            .pipe(rename({
+                basename: 'style',
+                suffix: '.min',
+            }))
+            .pipe(gulp.dest("NetCoreBB.Admin/wwwroot"))
     }
 }
